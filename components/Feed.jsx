a927143839fafd95027fbc2307a37bd72563ce8d
ZeroIDdev <115116337+ZeroIDdev@users.noreply.github.com>
2024-03-28 13:20:49 +0800

    Update Feed.jsx

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -36,7 +36,7 @@ const Feed = () => {
       try {
         const response = await fetch('/api/prompt')
         const data = await response.json()
-
+        console.log(data)
         setPost(data)
         
       } catch (error) {
